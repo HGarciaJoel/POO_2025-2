2160d1b46d803bfe5d7142c7748347915ba83ae2
GH_Joel <145246509+HGarciaJoel@users.noreply.github.com>
2025-03-20 18:50:08 -0600

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Un hola mundo en java
+ * Author: Garcia Hormiga Joel
+ */
+
+/**
+ * Main: imprime el hola mundo
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
